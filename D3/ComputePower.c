c4668f4801d1fe5038118494706d89e234b714a3
Deep Saha <dip23447@gmail.com>
2026-03-29 09:25:42 +0530

    Power function using recursion.

--- D3/ComputePower.c
+++ D3/ComputePower.c
@@ -9,7 +9,24 @@ int linearPower(int x, int n){
     return x * linearPower(x, n-1);
 }
 
+
+// Optimzed recursive Power function
+int powerFunction(int x, int n){
+    if(n == 0){
+        return 1;
+    }
+
+    int half = powerFunction(x, n/2);
+
+    if(n % 2 == 0){
+        return half * half;
+    }else{
+        return x * half * half;
+    }
+}
+
 int main(){
     int x = 2, n = 5;
-    printf("Linear Power: %d", linearPower(x, n));
+    // printf("Linear Power: %d", linearPower(x, n));
+    printf("Recursive Power: %d", powerFunction(x, n));
 }
