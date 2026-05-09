bd9970d867ec8a85ffa6c922280efa20a52cbbc5
Deep Saha <dip23447@gmail.com>
2026-05-09 09:58:02 +0530

    DFS function

--- D8/DFS.c
+++ D8/DFS.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+#define MAX 10
+
+void dfs(int graph[MAX][MAX], int visited[], int node, int vertices){
+    printf("%d ", node);
+    visited[node] = 1;
+
+    for(int i=0; i<vertices; i++){
+        if(graph[node][i] == 1 && visited[i] == 0){
+            dfs(graph, visited, i, vertices);
+        }
+    }
+}
